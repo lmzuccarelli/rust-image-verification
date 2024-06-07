c188cc566a28a928842bab6a938bb984bb123ac6
Luigi Mario Zuccarelli <luzuccar@redhat.com>
2024-06-07 15:03:37 +0200

    Add concurrency & dont evaluate duplicates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use custom_logger::*;
+use futures::stream::FuturesUnordered;
+use futures::stream::StreamExt;
 use serde_derive::{Deserialize, Serialize};
+use sha256::*;
 use std::env;
-use std::fs::{self};
+use std::fs::{self, read};
 use walkdir::WalkDir;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -62,7 +65,8 @@ pub struct Common {
     pub blob: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
 
     // setup logging
@@ -76,6 +80,12 @@ fn main() {
     //  get blobs directory
     let base_dir = &args[1].split("working-dir").nth(0).unwrap();
     let blobs_dir = format!("{}/working-dir/blobs-store/", &base_dir);
+    let mut futs = FuturesUnordered::new();
+    let batch_size = 16;
+    let mut blob_sum: String;
+    let mut blob_size: i64;
+    let mut manifest_config: Layer;
+    let mut vec_blobs: Vec<String> = vec![];
 
     for entry in WalkDir::new(&args[1]).into_iter().filter_map(Result::ok) {
         if entry.path().is_dir() {
@@ -91,36 +101,52 @@ fn main() {
             let manifest: Manifest =
                 serde_json::from_str(&data).expect("should be able to parse manifest");
 
-            let config_blob_sum = manifest
-                .config
-                .as_ref()
-                .unwrap()
-                .digest
-                .split(":")
-                .collect::<Vec<&str>>()[1];
-            let config_size = manifest.config.clone().unwrap().size;
+            manifest_config = manifest.clone().config.unwrap();
+            blob_size = manifest_config.clone().size;
+            blob_sum = manifest_config.digest.split(":").collect::<Vec<&str>>()[1].to_string();
 
-            // check for config size
-            let blob_file = format!(
-                "{}/{}/{}",
-                &blobs_dir,
-                &config_blob_sum[..2],
-                &config_blob_sum
-            );
-            let size = fs::metadata(&blob_file).unwrap().len();
-            log.hi(&format!("  config  blob {}", &config_blob_sum));
-            assert_eq!(size, config_size as u64);
+            // get blobs in batch as set by batch_size
+            // each future handles opens and verifies a file
+            // with 8 threads (one per digest)
+            // batch the calls
+            // push the config details first
+            futs.push(verify_file(log, blobs_dir.clone(), blob_sum, blob_size));
+
+            let layers = manifest.clone().layers.unwrap();
 
             // iterate through each components related layer
-            // and verify size
-            for layer in manifest.layers.unwrap().iter() {
+            // and verify sha256 contents and size
+            for layer in layers.iter() {
                 let blob_sum = layer.digest.split(":").collect::<Vec<&str>>()[1];
                 let blob_size = layer.size;
-                let layer_file = format!("{}/{}/{}", &blobs_dir, &blob_sum[..2], &blob_sum);
-                let size = fs::metadata(&layer_file).unwrap().len();
-                log.lo(&format!("  related blob {}", &blob_sum));
-                assert_eq!(size, blob_size as u64);
+                // don't re-evaluate duplicates
+                // saves time :)
+                if !vec_blobs.contains(&blob_sum.to_string()) {
+                    futs.push(verify_file(
+                        log,
+                        blobs_dir.clone(),
+                        blob_sum.to_string(),
+                        blob_size,
+                    ));
+                    vec_blobs.insert(0, blob_sum.to_string());
+                }
+                if futs.len() >= batch_size {
+                    let _response = futs.next().await.unwrap();
+                }
             }
+            // Wait for the remaining to finish.
+            while let Some(_response) = futs.next().await {}
         }
+    }
+
+    // verify_file - function to check size and sha256 hash of contents
+    async fn verify_file(log: &Logging, blobs_dir: String, blob_sum: String, blob_size: i64) {
+        let file = format!("{}/{}/{}", &blobs_dir, &blob_sum[..2], &blob_sum);
+        let size = fs::metadata(&file).unwrap().len();
+        log.lo(&format!("  related blob {}", &blob_sum));
+        assert_eq!(size, blob_size as u64);
+        let bytes = read(&file).unwrap();
+        let hash = digest(&bytes);
+        assert_eq!(hash, blob_sum);
     }
 }
